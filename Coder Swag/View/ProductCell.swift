e61281ab3b8a66d4ede2ce7587f3210c1abdb501
Asset Ryskul <asset89@gmail.com>
2020-08-14 14:09:23 +0600

    tableView complete & collectionView started

---
+++ Coder Swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Coder Swag
+//
+//  Created by Asset Ryskul on 8/13/20.
+//  Copyright © 2020 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
